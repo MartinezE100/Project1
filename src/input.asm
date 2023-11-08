fa800934df85f56e3388fa5ea2fa7317d2e10a99
Eduardo Martinez <edumart000@gmail.com>
2023-11-08 19:20:03 -0400

    added controller

---
+++ src/input.asm
@@ -0,0 +1,195 @@
+.include "constants.inc"
+.include "header.inc"
+
+.segment "ZEROPAGE"
+player_x: .res 1
+player_y: .res 1
+player_dir: .res 1
+pad1: .res 1
+.exportzp player_x, player_y, pad1
+
+.segment "CODE"
+.proc irq_handler
+  RTI
+.endproc
+
+.import read_controller1
+
+.proc nmi_handler
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
+  LDA #$00
+
+  ; read controller
+  JSR read_controller1
+
+  ; update tiles *after* DMA transfer
+  JSR update_player
+  JSR draw_player
+
+  LDA #$00
+  STA $2005
+  STA $2005
+  RTI
+.endproc
+
+.import reset_handler
+.import draw_background
+
+.export main
+.proc main
+  ; write a palette
+  LDX PPUSTATUS
+  LDX #$3f
+  STX PPUADDR
+  LDX #$00
+  STX PPUADDR
+load_palettes:
+  LDA palettes,X
+  STA PPUDATA
+  INX
+  CPX #$20
+  BNE load_palettes
+
+	; Nametables
+    JSR draw_background
+
+vblankwait:       ; wait for another vblank before continuing
+  BIT PPUSTATUS
+  BPL vblankwait
+
+  LDA #%10000000  ; turn on NMIs, sprites use first pattern table
+  STA PPUCTRL
+  LDA #%00011110  ; turn on screen
+  STA PPUMASK
+
+forever:
+  JMP forever
+.endproc
+
+.proc update_player
+  PHP  ; Start by saving registers,
+  PHA  ; as usual.
+  TXA
+  PHA
+  TYA
+  PHA
+
+  LDA pad1        ; Load button presses
+  AND #BTN_LEFT   ; Filter out all but Left
+  BEQ check_right ; If result is zero, left not pressed
+  DEC player_x  ; If the branch is not taken, move player left
+check_right:
+  LDA pad1
+  AND #BTN_RIGHT
+  BEQ check_up
+  INC player_x
+check_up:
+  LDA pad1
+  AND #BTN_UP
+  BEQ check_down
+  DEC player_y
+check_down:
+  LDA pad1
+  AND #BTN_DOWN
+  BEQ done_checking
+  INC player_y
+done_checking:
+  PLA ; Done with updates, restore registers
+  TAY ; and return to where we called this
+  PLA
+  TAX
+  PLA
+  PLP
+  RTS
+.endproc
+
+.proc draw_player
+  ; save registers
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  ; write player tile numbers
+  LDA #$29
+  STA $0201
+  LDA #$2a
+  STA $0205
+  LDA #$2b
+  STA $0209
+  LDA #$2c
+  STA $020d
+
+  ; write player tile attributes
+  ; use palette 0
+  LDA #$00
+  STA $0202
+  STA $0206
+  STA $020a
+  STA $020e
+
+  ; store tile locations
+  ; top left tile:
+  LDA player_y
+  STA $0200
+  LDA player_x
+  STA $0203
+
+  ; top right tile (x + 8):
+  LDA player_y
+  STA $0204
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $0207
+
+  ; bottom left tile (y + 8):
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $0208
+  LDA player_x
+  STA $020b
+
+  ; bottom right tile (x + 8, y + 8)
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $020c
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $020f
+
+  ; restore registers and return
+  PLA
+  TAY
+  PLA
+  TAX
+  PLA
+  PLP
+  RTS
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "RODATA"
+palettes:
+.byte $21, $16, $17, $20 ; main background pallete
+.byte $21, $00, $06, $30 
+.byte $21, $17, $28, $2a ; second tree pallete
+.byte $21, $09, $19, $29 ; first tree pallete
+
+.byte $21, $16, $17, $20
+.byte $21, $00, $06, $30
+.byte $21, $09, $19, $29
+.byte $21, $09, $19, $29
+
+.segment "CHR"
+.incbin "background.chr"
