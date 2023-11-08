9f64053964422d03951c5ddc3a1ae2e3225485e1
Eduardo Martinez <edumart000@gmail.com>
2023-11-08 17:02:35 -0400

    updated pallete colors

--- src/backgrounds.asm
+++ src/backgrounds.asm
@@ -2301,10 +2301,10 @@ forever:
 
 .segment "RODATA"
 palettes:
-.byte $21, $16, $17, $20
-.byte $21, $00, $06, $30
-.byte $21, $17, $28, $0f
-.byte $21, $09, $19, $29
+.byte $21, $16, $17, $20 ; main background pallete
+.byte $21, $00, $06, $30 
+.byte $21, $17, $28, $2a ; second tree pallete
+.byte $21, $09, $19, $29 ; first tree pallete
 
 .byte $21, $2d, $10, $15
 .byte $21, $09, $19, $29
